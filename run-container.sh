c497bc61850556c35322d67deff02ccacf818343
Egor4ik325 <zorinegor325@gmail.com>
2021-09-19 15:32:35 +0300

    Share source files with container while dev
    Won't work because MySQL is not configured for single container

--- run-container.sh
+++ run-container.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Create and start a container
+# Create and start a Django webapp container from project image
 docker container run \
     -p 127.0.0.1:8000:8000 \
+    -v "$(pwd)":/usr/app/src \
     -d \
     --name auction-webapp \
     auction-webapp
